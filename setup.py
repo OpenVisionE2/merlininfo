fde342ef70e23056726f53dd46153d0e5cf9a486
Persian Prince <persianpros@yahoo.com>
2019-07-15 08:46:37 +0430

    Follow our structure

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup (name = 'enigma2-plugin-extensions-merlininfo',
        version = '2.1',
        description = 'Plugin to show useful information in enigma2.',
        packages = [pkg],
-       package_dir = {pkg: 'usr'},
+       package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass = setup_translate.cmdclass, # for translation
       )
